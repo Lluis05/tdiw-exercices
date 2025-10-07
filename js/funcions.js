437808a991a12183ea5c4e12c5b014c3823398d3
Lluis Sarries <lssarries28@gmail.com>
2025-10-07 18:24:39 +0200

    New CSS and js. Ajax implemented

--- js/funcions.js
+++ js/funcions.js
@@ -5,3 +5,10 @@ function confirmaRegistre(){
     formulari.innerHTML = "<p class='important'>T'has registrat amb èxit!</p>";
     return false; // to block the form submission
 }
+
+async function carregaMencions(){
+    var tagGraus = document.getElementById("graus");
+    var resposta = await fetch("mencions.php?grau="+tagGraus.value);
+    var respostaTxt = await resposta.text();
+    document.getElementById("mencions").innerHTML = respostaTxt;
+}
